b9c2f2cbda25c27cfd8434cc65018d0a09b0d8b8
Sean <ussa.say@gmail.com>
2017-09-19 20:47:43 -0700

    pls.... i need hype

--- tiles.js
+++ tiles.js
@@ -1,7 +1,42 @@
-var tiles=[
-  {
-    name:"house",
-    texture:[0,0],
-    colour:'#4bbf43'
+var tiles=[];
+tiles.add=(name,colour,x,y)=>{
+  var r={name:name,colour:colour};
+  if (x!==undefined&&y!==undefined) r.texture=[x,y];
+  tiles.push(r);
+};
+function renderTiles(c,textures,settings,grid,offsetx=0,offsety=0) {
+  var x=Math.floor(offsetx/settings.TILE_SIZE),
+  y=Math.floor(offsety/settings.TILE_SIZE);
+  offsetx=x*settings.TILE_SIZE-offsetx,
+  offsety=y*settings.TILE_SIZE-offsety;
+  var width=settings.STAGE_WIDTH/settings.TILE_SIZE+(offsetx!==0),
+  height=settings.STAGE_HEIGHT/settings.TILE_SIZE+(offsety!==0),
+  t;
+  for (var i=0;i<width;i++) for (var j=0;j<height;j++) {
+    if ((t=tiles[grid[`${i+x},${j+y}`]])!==undefined) {
+      if (t.colour) {
+        c.fillStyle=t.colour;
+        c.fillRect(i*settings.TILE_SIZE+offsetx,j*settings.TILE_SIZE+offsety,settings.TILE_SIZE,settings.TILE_SIZE);
+      }
+      if (t.texture)
+        c.drawImage(textures,t.texture[0]*settings.TILE_SIZE,t.texture[1]*settings.TILE_SIZE,settings.TILE_SIZE,settings.TILE_SIZE,i*settings.TILE_SIZE+offsetx,j*settings.TILE_SIZE+offsety,settings.TILE_SIZE,settings.TILE_SIZE);
+    }
   }
-];
+}
+
+/*
+   _____                   _         _                     _     _   _                  _
+  |  __ \                 (_)       | |                   | |   (_) | |                | |
+  | |__) |   ___    __ _   _   ___  | |_    ___   _ __    | |_   _  | |   ___   ___    | |__     ___   _ __    ___
+  |  _  /   / _ \  / _` | | | / __| | __|  / _ \ | '__|   | __| | | | |  / _ \ / __|   | '_ \   / _ \ | '__|  / _ \
+  | | \ \  |  __/ | (_| | | | \__ \ | |_  |  __/ | |      | |_  | | | | |  __/ \__ \   | | | | |  __/ | |    |  __/
+  |_|  \_\  \___|  \__, | |_| |___/  \__|  \___| |_|       \__| |_| |_|  \___| |___/   |_| |_|  \___| |_|     \___|
+                   __/ |
+                  |___/
+  Register tiles here
+
+  Use tiles.add to add in the tiles. Please don't change the order!
+*/
+tiles.add('grass','#4bbf43');
+tiles.add('house','#4bbf43',0,0);
+tiles.add('tree','#4bbf43',1,0);
--- window.js
+++ window.js
@@ -1,8 +1,10 @@
 var settings={
   STAGE_WIDTH:400,
-  STAGE_HEIGHT:225
+  STAGE_HEIGHT:225,
+  TILE_SIZE:25
 },
-render={};
+render={},
+map={};
 window.addEventListener("load",e=>{
   function resizeCanvas() {
     var w,h; // if screen too small, resize proportionally to fit
@@ -14,14 +16,22 @@ window.addEventListener("load",e=>{
     render.canvas.height=h*render.pxr;
     render.canvas.style.height=h+'px';
     render.c.scale(render.pxr,render.pxr);
-    render.c.fillStyle='#333';
     render.c.fillRect(0,0,render.canvas.width,render.canvas.height);
   }
   var c;
   render.textures=document.getElementById('textures'),
   render.canvas=document.getElementById('stage'),
   render.c=c=render.canvas.getContext('2d'),
-  render.pxr=(window.devicePixelRatio||1)/(c.webkitBackingStorePixelRatio||c.mozBackingStorePixelRatio||c.msBackingStorePixelRatio||c.oBackingStorePixelRatio||c.backingStorePixelRatio||1);
+  // render.pxr=(window.devicePixelRatio||1)/(c.webkitBackingStorePixelRatio||c.mozBackingStorePixelRatio||c.msBackingStorePixelRatio||c.oBackingStorePixelRatio||c.backingStorePixelRatio||1);
+  render.pxr=1; // mm
   window.addEventListener("resize",resizeCanvas,false);
   resizeCanvas();
+  for (var i=0;i<160;i++) map[`${Math.floor(i/9)},${i%9}`]=Math.floor(Math.random()*3);
+  scroll=[0,0];
+  function draw() {
+    renderTiles(render.c,render.textures,settings,map,scroll[0],scroll[1]);
+    c.drawImage(textures,2*settings.TILE_SIZE,0,settings.TILE_SIZE,settings.TILE_SIZE,(render.canvas.width-settings.TILE_SIZE)/2,(render.canvas.height-settings.TILE_SIZE)/2,settings.TILE_SIZE,settings.TILE_SIZE);
+    // window.requestAnimationFrame(draw);
+  }
+  window.requestAnimationFrame(draw);
 },false);
